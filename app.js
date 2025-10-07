f2ba649f279d21ed960b4f70ec575c8be5a7fee3
FallenMAD <naruto.yzymaki4@gmail.com>
2025-10-07 20:04:12 +0200

    refactor(app.js): use error controller instead of raw fn

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ import { rootDir } from './utils/dirnameHelper.js';
  
 import adminRoutes from './routes/admin.js';
 import shopRoutes from './routes/shop.js';
+import { errorController } from './controllers/error.js';
 
 const app = express();
 
@@ -17,8 +18,6 @@ app.use(express.static(path.join(rootDir, 'public')));
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-  res.status(404).render('404', { docTitle: 'Not Found Page' });
-});
+app.use(errorController.handle404);
 
 app.listen(3003);
