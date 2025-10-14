e39a401c6bf781f8c0420c0e17922720794d1e49
FallenMAD <naruto.yzymaki4@gmail.com>
2025-10-14 19:57:49 +0200

    refactor(product.js): update main logic of file

--- models/product.js
+++ models/product.js
@@ -2,21 +2,26 @@ import path from 'path'
 import fs from 'fs';
 import { rootDir } from '../utils/dirnameHelper.js';
 
+const p = path.join(rootDir, 'data', 'products.json')
+
+const getProductsFromFile = (cb) => {
+  return fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      return cb([]);
+    }
+    
+    cb(JSON.parse(fileContent))
+  })
+}
+
 export class Product {
   constructor(title) {
     this.title = title
   }
 
   save() {
-    const p = path.join(rootDir, 'data', 'products.json')
-
-    fs.readFile(p, (err, fileContent) => {
-      let products = [];
-      
-      if (!err) {
-        products = JSON.parse(fileContent)
-      }
-
+    getProductsFromFile((products) => {
+      console.log(products)
       products.push(this);
 
       fs.writeFile(p, JSON.stringify(products), (err) => {
@@ -25,17 +30,7 @@ export class Product {
     })
   }
 
-  static getAllProducts(cd) {
-    const p = path.join(rootDir, 'data', 'products.json')
-    // let products = []
-    
-    return fs.readFile(p, (err, fileContent) => {
-      if (err) {
-        cd([]);
-      }
-      
-      cd(JSON.parse(fileContent))
-      
-    })
+  static getAllProducts(cb) {
+    getProductsFromFile(cb)
   }
 }
