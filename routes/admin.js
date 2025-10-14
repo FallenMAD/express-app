c9aff2cbd9cba155cc80433cee5260aa9ea9b926
FallenMAD <naruto.yzymaki4@gmail.com>
2025-10-14 19:59:10 +0200

    style(admin.js & shop.js): update naming

--- routes/admin.js
+++ routes/admin.js
@@ -5,6 +5,6 @@ const router = express.Router();
 
 router.get('/add-product', productsController.getAddProduct)
 
-router.post('/add-product', productsController.addProduct)
+router.post('/add-product', productsController.postAddProduct)
 
 export default router;
--- routes/shop.js
+++ routes/shop.js
@@ -4,6 +4,6 @@ import { productsController } from '../controllers/products.js';
 
 const router = express.Router();
 
-router.get('/', productsController.getProduct);
+router.get('/', productsController.getProducts);
 
 export default router;
