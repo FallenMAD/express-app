3ffc6ee54ad688ca0f31385b3e1242961a9049f0
FallenMAD <naruto.yzymaki4@gmail.com>
2025-11-03 15:50:39 +0100

    feature(admin): add new edit controller handlers

--- controllers/admin.js
+++ controllers/admin.js
@@ -2,16 +2,44 @@ import { Product } from '../models/product.js';
 
 export const adminController = {
   getAddProduct(req, res, next) {
-    res.render('admin/add-product', {
+    res.render('admin/edit-product', {
       docTitle: 'Add Product',
       path: req.originalUrl,
+      editing: false,
     });
   },
 
   postAddProduct(req, res, next) {
     const { title, imageUrl, price, description } = req.body;
-    const product = new Product(title, imageUrl, description, price);
-    product.save(product);
+    const product = new Product(null, title, imageUrl, description, price);
+    product.save();
+    res.redirect('/');
+  },
+
+  getEditProduct(req, res, next) {
+    const { id } = req.params;
+    const isEditMode = req.query.edit;
+    if (!isEditMode) {
+      return res.redirect('/');
+    }
+
+    Product.findById(id, (product) => {
+      if (!product) {
+        return res.redirect('/');
+      }
+      res.render('admin/edit-product', {
+        docTitle: 'Editing Product',
+        path: req.originalUrl,
+        editing: isEditMode,
+        product: product,
+      });
+    });
+  },
+
+  postEditProduct(req, res, next) {
+    const { id, title, imageUrl, price, description } = req.body;
+    const product = new Product(id, title, imageUrl, description, price);
+    product.save();
     res.redirect('/');
   },
 
