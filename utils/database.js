077aa9c40e39186ac0dae37a2e3ba4969a7d5ffa
FallenMAD <naruto.yzymaki4@gmail.com>
2025-11-09 16:01:04 +0100

    feature(database): create db and env example

--- utils/database.js
+++ utils/database.js
@@ -1,8 +1,13 @@
 import mysql from 'mysql2';
+import dotenv from 'dotenv';
 
-export const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'node-complete',
-    password: 'Romanqwert12345',
-}).promise();
+dotenv.config();
+
+export const pool = mysql
+  .createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+  })
+  .promise();
