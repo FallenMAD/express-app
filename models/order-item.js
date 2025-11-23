48a1aeb0992e0f43273cdced904a6eb396fc4e9d
FallenMAD <naruto.yzymaki4@gmail.com>
2025-11-23 20:59:47 +0100

    feature(order): create order and order item model

---
+++ models/order-item.js
@@ -0,0 +1,11 @@
+import { DataTypes } from 'sequelize';
+import { sequelize } from '../utils/database.js';
+
+export const OrderItem = sequelize.define('OrderItem', {
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+});
